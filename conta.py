3b1e3dbd4e2eb9884c0cd28bd84bab745cd205ac
João Victor <jvav2004@hotmail.com>
2023-03-30 16:21:57 -0300

    primeiro commit

---
+++ conta.py
@@ -0,0 +1,49 @@
+class Conta:
+
+    def init(self, numero, titular, saldo, limite):
+        print(f"Construindo objeto {self}")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+
+    def extrato(self):
+        print(f"Saldo de {self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__limite + self.__saldo
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+    def saca(self, valor):
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou o limite!")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
